4470ed3ebc87b0785749b4ef57cc141d66d6f25a
Alex Wilson <a@ax.gy>
2016-01-02 22:26:19 +0000

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+import * as tumblr from "tumblr";
+import fetch from "node-fetch";
+
+function repost(item) {
+  console.log("WHEEE", item);
+};
+
+const date = new Date();
+const startDate = new Date(date.getTime() - (600 * 1000));
+
+const tags = [
+  'komeiji_koishi',
+  'score:>1',
+  'date:'+startDate.toISOString()+'..'+date.toISOString()
+];
+
+fetch(
+    'https://danbooru.donmai.us/posts.json?tags=' + tags.join(' '),
+    {
+      method: 'get',
+    }
+  )
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    res.forEach(repost);
+  });
