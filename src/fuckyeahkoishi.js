ba57c41bac42d76a4fc23e8cf8c037d499412357
Alex Wilson <a@ax.gy>
2016-01-03 00:47:42 +0000

    use redis to keep track of when we were last run - also use env variables

--- src/fuckyeahkoishi.js
+++ src/fuckyeahkoishi.js
@@ -1,16 +1,38 @@
 import * as tumblr from "tumblr";
 import fetch from "node-fetch";
+import redis from "redis";
 
 export default function() {
+
+  console.log('Starting FuckYeahKoishi');
+
+  let auth = '';
+  if (process.env.DANBOORU_USER && process.env.DANBOORU_KEY) {
+    console.log('Danbooru credentials found.');
+    auth = new Buffer(process.env.DANBOORU_USER+':'+process.env.DANBOORU_KEY).toString('base64');
+  }
+
+  const date = new Date();
+  const startDate = new Date(date.getTime() - (600 * 1000));
+
+  // Do we have persistence?
+  if (process.env.REDIS_URL) {
+    console.log('Redis found - ' + process.env.REDIS_URL);
+    const client = redis.createClient(process.env.REDIS_URL);
+    const redisDate = client.get('lastRun', (err, reply) => {
+      console.log('Last run time: ' + reply);
+      return reply;
+    });
+    if (redisDate !== null) {
+      const startDate = redisDate;
+    }
+    client.set('lastRun', date.getTime());
+    client.end();
+  }
 
   function repost(item) {
     console.log("WHEEE", item);
   };
-
-  const date = new Date();
-  const startDate = new Date(date.getTime() - (600000 * 1000));
-
-  const auth = new Buffer("albert:SSB3YW50IGEgbW9uc3RlciBjb2NrIGluIG15IG1vdXRo").toString('base64');
 
   const tags = [
     'komeiji_koishi',
@@ -31,6 +53,7 @@ export default function() {
       return res.json();
     })
     .then((res) => {
+      console.log('Found ' + res.length + ' new results.');
       res.forEach(repost);
     });
 
